a5f1c7e80d615b1df93cdd97b13ff5001b1ab9df
alanplayoficial <143126887+alanplayoficial@users.noreply.github.com>
2023-11-25 13:45:19 -0300

    Create install packs.js
    packs maps chat events and more
    
    Signed-off-by: alanplayoficial <143126887+alanplayoficial@users.noreply.github.com>

---
+++ .github/workflows/install packs.js
@@ -0,0 +1,29 @@
+const miTitulo = document.querySelector("h1");
+miTitulo.textContent = "¡Hola mundo!";
+let nombreDeLaVariable;
+nombreDeLaVariable = "Bob";
+let nombreDeLaVariable = "Bob";
+nombreDeLaVariable;
+let nombreDeLaVariable = "Bob";
+nombreDeLaVariable = "Steve";
+let helado = "chocolate";
+if (helado === "chocolate") {
+  alert("¡Sí, amo el helado de chocolate!");
+} else {
+  alert("Awwww, pero mi favorito es el de chocolate...");
+}
+let nombreDeLaVariable = document.querySelector("h1");
+alert("¡Hola!");
+function multiplica(num1, num2) {
+  let resultado = num1 * num2;
+  return resultado;
+}
+multiplica(4, 7);
+multiplica(20, 20);
+multiplica(0.5, 3);
+document.querySelector("html").onclick = function () {
+  alert("¡Ouch! ¡Deja de pincharme!");
+};
+document.querySelector("html").onclick = function () {};
+let miHTML = document.querySelector("html");
+miHTML.onclick = function () {};
